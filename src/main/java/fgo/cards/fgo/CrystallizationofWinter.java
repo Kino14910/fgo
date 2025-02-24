e4a28237e03373a7d0bdfec868a740fce696e5cc
kino <1491037864@qq.com>
2025-02-24 23:24:18 +0800

    Revert "delete Crytallization of Winter in fgo card"
    This reverts commit 1109c3eca6e7bc3f17d304591d1d0db8eebd3fd0.

---
+++ src/main/java/fgo/cards/fgo/CrystallizationofWinter.java
@@ -0,0 +1,36 @@
+package fgo.cards.fgo;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import fgo.cards.FGOCard;
+import fgo.patches.Enum.FGOCardColor;
+import fgo.powers.ComeOnPower;
+import fgo.powers.GutsPower;
+import fgo.util.CardStats;
+
+public class CrystallizationofWinter extends FGOCard {
+    public static final String ID = makeID(CrystallizationofWinter.class.getSimpleName());
+    private static final CardStats INFO = new CardStats(
+            FGOCardColor.FGO,
+            CardType.POWER,
+            CardRarity.SPECIAL,
+            CardTarget.SELF,
+            0
+    );
+    public static final String IMG = "fgo/images/cards/power/ConjureIce.png";
+    public CrystallizationofWinter() {
+        super(ID, INFO, IMG);
+        setMagic(4, 1);
+
+    }
+
+
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new GutsPower(p, 10, 1), 10));
+        //this.addToBot(new ApplyPowerAction(p, p, new TheOneWhoRevealsTruthPower(p)));
+        this.addToBot(new ApplyPowerAction(p, p, new ComeOnPower(p, this.magicNumber), this.magicNumber));
+    }
+}
