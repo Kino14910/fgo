d8d49d68fcf3dc1c376e80e5a684f73ffb835dbe
kino <1491037864@qq.com>
2026-02-25 00:49:30 +0800

    Update FifthFormPower.java

--- src/main/java/fgo/powers/FifthFormPower.java
+++ src/main/java/fgo/powers/FifthFormPower.java
@@ -2,16 +2,14 @@ package fgo.powers;
 
 import static fgo.FGOMod.makeID;
 
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardTarget;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
-import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class FifthFormPower extends BasePower {
@@ -32,26 +30,12 @@ public class FifthFormPower extends BasePower {
 
     @Override
     public void onUseCard(AbstractCard card, UseCardAction action) {
-        if (!card.purgeOnUse && card.type == CardType.ATTACK && (card.target == CardTarget.ENEMY || card.target == CardTarget.SELF_AND_ENEMY)) {
-            flash();
-            AbstractMonster m = null;
-            if (action.target != null) {
-                m = (AbstractMonster)action.target;
-            }
-
-            AbstractCard tmp = card.makeSameInstanceOf();
-            AbstractDungeon.player.limbo.addToBottom(tmp);
-            tmp.current_x = card.current_x;
-            tmp.current_y = card.current_y;
-            tmp.target_x = Settings.WIDTH / 2.0f - 300.0f * Settings.scale;
-            tmp.target_y = Settings.HEIGHT / 2.0f;
-            if (m != null) {
-                tmp.calculateCardDamage(m);
-            }
-
-            tmp.purgeOnUse = true;
-            AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true), true);
+        if (card.purgeOnUse || card.type != CardType.ATTACK || (card.target != CardTarget.ENEMY && card.target != CardTarget.SELF_AND_ENEMY) || action.target == null) {
+            return;
         }
+        flash();
+        AbstractMonster m = (AbstractMonster)action.target;
+        GameActionManager.queueExtraCard(card, m);
     }
 
     @Override
